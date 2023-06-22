e349d4d50d5eaa6ad957abc42002aa59e53782a5
themanspider <137458977+themanspider@users.noreply.github.com>
2023-06-22 17:53:33 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function starClassification()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https//:teachablemachine.withgoogle.com/models/lfFaG5_io/model.json', modelReady);
+}
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+ if(error) {
+    console.error(error);
+    }else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'Escucha - '+ results(0).label;
+        document.getElementById("result_label").innerHTML = 'Presicion - '+ (results(0).confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r", "+random_number_g", "+random_number_b")";
+
+        img = document.getElementById('alien1')
+        img1 = document.getElementById('alien2')
+        img2 = document.getElementById('alien3')
+        img3 = document.getElementById('alien4')
+
+        if (results(0).label == "Aplausos") {
+            img.src = 'aliens-01.gif';
+            img.src = 'aliens-02.png';
+            img.src = 'aliens-03.png';
+            img.src = 'aliens-04.png';
+        }else if (results(0).label == "Chasquido") {
+            img.src = 'aliens-01.png';
+            img1.src = 'aliens-02.gif';
+            img2.src = 'aliens-03.png';
+            img3.src = 'aliens-04.png';
+        }else if (results(0).label == "Campana") {
+            img.src = 'aliens-01.png';
+            img1.src = 'aliens-02.png';
+            img2.src = 'aliens-03.gif';
+            img3.src = 'aliens-04.png';
+        }
+    }
+ }
